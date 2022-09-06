1ffddbbfebb67751ab84567da34c508e7f6dc7af
CristianMar2 <cristianmarfer14@gmail.com>
2022-09-06 08:17:18 -0300

    Dia 06/09

--- src/Lista2Sem/BatalhaNaval.java
+++ src/Lista2Sem/BatalhaNaval.java
@@ -7,25 +7,28 @@ public class BatalhaNaval
 {
     public static final int tamanhoTab = 10;
     public static final int tamanhoBarco = 3;
-    public static final int quantidadeBarco = 3;
-    public static char[][] tabuleiro;
+    public static final int quantidadeBarco = 6;
+    public static char[][] tabuleiro, tabuleiro2;
     public static int linha, coluna;
     public static Scanner ler = new Scanner(System.in);
+    public static int turno;
 
     public static void criarTabuleiro() {
         tabuleiro = new char[tamanhoTab][tamanhoTab];
+        tabuleiro2 = new char[tamanhoTab][tamanhoTab];
         for (int i = 0; i < tamanhoTab; i++)
         {
             Arrays.fill(tabuleiro[i], '°');
+            Arrays.fill(tabuleiro2[i], '°');
         }
     }
     public static void mostrarTabuleiro() {
         int i;
         for (i = 0; i < tamanhoTab; i++)
-            System.out.printf("\t" + i);
+            System.out.printf("\t" + (i + 1));
         System.out.println("");
         for (i = 0; i < tamanhoTab; i++) {
-            System.out.print(i + " |\t");
+            System.out.print((char) (i + 65) + " |\t");
             for (int j = 0; j < tamanhoTab; j++) {
                 if (tabuleiro[i][j] == '@')
                     System.out.print("º | ");
@@ -34,7 +37,7 @@ public class BatalhaNaval
             System.out.println("");
         }
     }
-    public static void mostarJogoComeçando() throws InterruptedException {
+    public static void mostrarJogoComeçando() throws InterruptedException {
         System.out.println("O jogo está começando, espere um momento enquanto os barcos são colocados!");
         Thread.sleep(3000);
     }
@@ -101,11 +104,16 @@ public class BatalhaNaval
     }
 
     public static void main(String[] args) throws InterruptedException {
-        criarTabuleiro();
-        mostarJogoComeçando();
-        criarBarco();
-        mostrarTabuleiro();
-        iniciarJogo();
+        short novoJogo = 0;
+        do {
+            criarTabuleiro();
+            mostrarJogoComeçando();
+            criarBarco();
+            mostrarTabuleiro();
+            iniciarJogo();
+            System.out.println("Deseja jogar novamente ? (Digite 1 para Sim ou qualquer outro número para não)");
+        }while(novoJogo == 1);
+        System.out.println("Obrigado por jogar!!");
     }
     public static void iniciarJogo() throws InterruptedException {
         int rodada = 0;
@@ -113,26 +121,26 @@ public class BatalhaNaval
 
         do{
             lerCoordenadas();
-            System.out.println("Tentativa: \n" + ++rodada);
+            System.out.println("Tentativa: " + ++rodada + "\n");
             jogarBomba();
             if (tabuleiro[linha][coluna] == '*') partesNavios++;
         }while(partesNavios < tamanhoBarco * quantidadeBarco);
-        JOptionPane.showMessageDialog(null, "Meus parabéns!! Você destruiu todos od barcos em " + rodada + " tentativas!");
+        System.out.println("Meus parabéns!! Você destruiu todos os barcos em " + rodada + " tentativas!");
     }
     public static void lerCoordenadas(){
         do {
-            System.out.println("Digite as coordenadas separadas por espaço (Exemplo: 2 3): ");
-            linha = ler.next().toUpperCase(Locale.ROOT).charAt(0) - 48;
-            coluna = ler.next().charAt(0) - 48;
+            System.out.println("\nDigite as coordenadas separadas por espaço (Exemplo: C 3): ");
+            linha = ler.next().toUpperCase(Locale.ROOT).charAt(0) - 65;
+            coluna = ler.next().charAt(0) - 49;
         }while (coordenadasValidas());
     }
     public static boolean coordenadasValidas(){
         if (linha < 0 || linha >= tamanhoTab || coluna < 0 || coluna >= tamanhoTab){
-            JOptionPane.showMessageDialog(null, "Coordenadas inválidas!");
+            System.out.println("Coordenadas inválidas!");
             return true;
         }
         if (tabuleiro[linha][coluna] == '~' || tabuleiro[linha][coluna] == '*'){
-            JOptionPane.showMessageDialog(null, "Esta casa já foi detonada!");
+            System.out.println("Esta casa já foi detonada!");
             return true;
         }
         return false;
